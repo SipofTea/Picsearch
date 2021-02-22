5ce8548f1f641f9d100f47a7018a47308e616203
SipofTea <p.e.meijerink@gmail.com>
2021-02-22 15:55:16 +0000

    Created imageList function and implemented in app. Pictures are now displayed in grid using css styling.

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SearchBar from "./searchbar";
 import unsplash from "../api/unsplash";
-
+import ImageList from "./imageList";
 class App extends React.Component {
   state = { images: [] };
 
@@ -9,7 +9,7 @@ class App extends React.Component {
     const response = await unsplash.get("/search/photos", {
       params: { query: term },
     });
-    
+
     this.setState({ images: response.data.results });
   };
 
@@ -17,7 +17,7 @@ class App extends React.Component {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
-        Found: {this.state.images.length} images
+        <ImageList images={this.state.images}></ImageList>
       </div>
     );
   }
